fda0f2165cf292d25dcee1e583d3cc9190a91d50
dklinh05 <dklinh05@gmail.com>
2026-03-02 00:33:35 +0700

    FSS-22: apply vouchers

--- src/routes/location.route.ts
+++ src/routes/location.route.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { receiveLocation } from '../controllers/location.controller';
 
 const router = Router();
-
 router.post("/", receiveLocation);
 
 export default router;
