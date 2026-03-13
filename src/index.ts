58f4545e01844eee8302e7464104ed16547adb5f
nta1210 <anhkn7@gmail.com>
2026-03-13 22:25:12 +0700

    add alias config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -15,10 +16,10 @@ const app = express();
 //middleware
 const allowedOrigins = [
   APP_ORIGIN,
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:5173",
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://127.0.0.1:3000',
+  'http://127.0.0.1:5173',
 ];
 
 const corsOptions: cors.CorsOptions = {
@@ -29,11 +30,11 @@ const corsOptions: cors.CorsOptions = {
     callback(new Error(`CORS blocked: ${origin}`));
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(customResponse);
 app.use(cookieParser());
@@ -43,7 +44,7 @@ app.use(express.urlencoded({ extended: true }));
 //app routes
 app.use('/api', appRoutes);
 
-// error handler 
+// error handler
 app.use(errorHandler);
 
 const server = http.createServer(app);
@@ -52,7 +53,7 @@ const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST"],
+    methods: ['GET', 'POST'],
   },
 });
 
