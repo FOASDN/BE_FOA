ddab754e5deaff2595da08ea90c64a02778c79db
dklinh05 <dklinh05@gmail.com>
2026-03-22 18:37:17 +0700

    fix cartcart

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,7 +1,16 @@
 import { OK } from '@/constants/http';
 import { catchErrors } from '@/utils/asyncHandler';
-import { addToCart } from '@/services/cart.service';
+import { addToCart, getCart } from '@/services/cart.service';
 import { addToCartValidator } from '@/validators/cart.validator';
+
+export const getCartHandler = catchErrors(async (req, res) => {
+  const user = req.userId;
+  const cart = await getCart(user);
+
+  return res.success(OK, {
+    data: cart,
+  });
+});
 
 export const addToCartHandler = catchErrors(async (req, res) => {
   const user = req.userId;
